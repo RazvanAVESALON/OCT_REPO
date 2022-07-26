073a43a46d4aa2a260a8ab6f3e8cebed935665cb
Razvan <razave81@gmail.com>
2022-07-26 14:54:23 +0300

    Update Graphic_maker.py

--- Graphic_maker.py
+++ Graphic_maker.py
@@ -8,22 +8,22 @@ data= pd.read_csv(r'D:\ai intro\OCT\OCT_file\Statistica.csv')
      
 Dice=pd.read_csv(r'D:\ai intro\OCT\OCT_REPO\DICE_ADDED.csv')      
 #plt.hist(data['aria'],[0.10000,20000,30000,40000,50000,60000,70000,80000,90000])  
-plt.hist(data['aria'], 20)  
-plt.title('ARIA')
-plt.xlabel("Value")
-plt.ylabel("Count")
-plt.savefig(r"D:\ai intro\OCT\OCT_REPO\Histograma-Arie")   
-
-
-
-
-# print (Dice['dice'])
-# plt.hist(Dice['dice'] ,[-1,0,1])
-
-# plt.title("DICE INDEX ")
+# plt.hist(data['aria'], 20)  
+# plt.title('ARIA')
 # plt.xlabel("Value")
 # plt.ylabel("Count")
-# plt.savefig(r'D:\ai intro\OCT\OCT_REPO\Histograma_Dice'  )
+# plt.savefig(r"D:\ai intro\OCT\OCT_REPO\Histograma-Arie")   
+
+
+
+
+print (Dice['dice'])
+plt.hist(Dice['dice'] ,[0,1])
+
+plt.title("DICE INDEX ")
+plt.xlabel("Value")
+plt.ylabel("Count")
+plt.savefig(r'D:\ai intro\OCT\OCT_REPO\Histograma_Dice'  )
            
 
 
